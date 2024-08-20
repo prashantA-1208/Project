cbfb975722177a07a23a665c6b57c293e9124768
Prashant <aryanprashant123@gmail.com>
2024-08-20 17:43:05 +0530

    Till the creation of server and Before Creating Client(React)

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,5 +1,6 @@
 import express from "express";
-import {registerController} from "../controllers/authController.js";  
+import {registerController,loginController,testController} from "../controllers/authController.js";  
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
 
 
 //router object
@@ -9,5 +10,14 @@ const router = express.Router()
 //Register || Method Post
 
 router.post('/register' , registerController);
+
+
+//Login || Post
+
+router.post("/login",loginController);
+
+//test route
+
+router.get("/test" , requireSignIn , isAdmin, testController)
 
 export default router
